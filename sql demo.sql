1faddccd0f6096e1f877f6cecf4afc641dd5b601
AshleyBairi <slexibairi144@gmail.com>
2026-03-02 19:48:09 -0600

    Add files via upload

---
+++ sql demo.sql
@@ -0,0 +1,12 @@
+select *
+From parks_and_recreation.employee_demographics;
+
+select first_name,
+last_name,
+birth_date,
+age,
+age+10
+From parks_and_recreation.employee_demographics;
+
+select distinct first_name,gender
+From parks_and_recreation.employee_demographics;
